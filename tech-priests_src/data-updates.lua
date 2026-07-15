089f823e38c8281cb49e63f695d62a11b416700f
mrcalzon02 <mrcalzon02@yahoo.com>
2026-07-15 09:03:26 -0800

    Load custom thruster mounting corrections

--- tech-priests_src/data-updates.lua
+++ tech-priests_src/data-updates.lua
@@ -13,4 +13,10 @@ if mods["factoryplanner"] then
   require("prototypes.compatibility.factoryplanner")
 end
 
+if mods["space-age"] then
+  -- Apply custom thruster mounting geometry and restore translated vanilla
+  -- pipe-connection visualisations after the primary Space Age prototypes exist.
+  require("prototypes.compatibility.thruster-mounting-visuals")
+end
+
 require("prototypes.gui_inner_styles_0635")
